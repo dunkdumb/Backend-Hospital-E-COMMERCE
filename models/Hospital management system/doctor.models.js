ed37bef30f212b213413dbd6d11c4785ff735fcd
dunkdumb <enosuppada2005@gmail.com>
2025-12-04 10:32:51 +0530

    hospital and other

--- models/Hospital management system/doctor.models.js
+++ models/Hospital management system/doctor.models.js
@@ -1,5 +1,41 @@
 import mongoose, { mongo } from "mongoose"
+import { required } from "zod/v4-mini"
 
-doctor_Schema= new mongoose.Schema({},{timestamps:true})
+doctor_Schema= new mongoose.Schema({
+    name :{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    workingin_hospitals:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"hospital"
+    },
+    salary :{
+        type:Number,
+        required:false
+    },
+    specialization:{
+        type:String,
+        enum:["Cardiology", "Dermatology", "Neurology", "Endocrinology", "Gastroenterology", "Pediatrics", "Oncology", "Orthopedics"] ,
+        default :["general surgeon"] 
+    },
+    qualification:{
+        type :String,
+        enum:[
+                "NEET-UG" ,
+                "MBBS" ,
+                "Medical Registration" ,
+                "NEET-PG"  ,
+                "MD",
+            ],
+        default:["lab technician"]
+    }
+
+    
+},{timestamps:true})
 
 export const doctors = mongoose.model("doctors",doctor_Schema)
--- models/Hospital management system/hospital.models.js
+++ models/Hospital management system/hospital.models.js
@@ -1,5 +1,23 @@
 import mongoose, { mongo } from "mongoose"
+import { required } from "zod/v4-mini"
 
-hospital_Schema= new mongoose.Schema({},{timestamps:true})
+hospital_Schema= new mongoose.Schema({
+    name : {
+        type :String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    no_of_doctors:{
+
+    },
+    specialized_in:[
+        {
+            type:String
+        }
+    ]
+},{timestamps:true})
 
 export const hospital = mongoose.model("hospital",hospital_Schema)
